77721817fa5f324b19134315f908282200ad4aba
fredbeaupre <fred.beaupre32@icloud.com>
2022-03-25 09:38:43 -0400

    gitignore, figure generator, participant directory

--- figure_generator.py
+++ figure_generator.py
@@ -4,6 +4,55 @@ import scipy.stats as stats
 plt.style.use('dark_background')
 
 
+def fix_mistakes(name):
+    data = np.load('{}.npz'.format(name))
+    fp_files = data['fp_errors']
+    fn_files = data['fn_errors']
+    classifications = data['classifications']
+    print(classifications[173, :])
+    print(classifications[365, :])
+    classifications[173, 0] = 1
+    classifications[365, 0] = 1
+    new_fn_files = []
+    for f in fn_files:
+        print(f)
+        print(f == './actin/test/207-0.002.npz' or f ==
+              './actin/test/169-0.019.npz')
+        if f == './actin/test/207-0.002.npz' or f == './actin/test/169-0.019.npz':
+            continue
+        else:
+            new_fn_files.append(f)
+    np.savez('./{}'.format('Flavie_fixed.npz'),
+             classifications=classifications,
+             fp_errors=fp_files,
+             fn_errors=new_fn_files)
+
+
+def check_errors(name):
+    data = np.load('{}.npz'.format(name))
+    fp_files = data['fp_errors']
+    fn_files = data['fn_errors']
+    for i in range(len(fn_files)):
+        fig = plt.figure()
+        f = fn_files[i]
+        img_file = np.load(f)
+        quality = f.split('/')[-1].split('-')[-1][:-4]
+        img = img_file['arr_0']
+        plt.title('Classified as normal, Quality rating = {}'.format(quality))
+        plt.xlabel('File name: {}'.format(f))
+        plt.imshow(img, cmap='hot')
+        plt.show()
+        # fig.savefig('./Flavie/false_negatives/FN_{}.png'.format(i))
+    for f in fp_files:
+        img_file = np.load(f)
+        quality = f.split('/')[-1].split('-')[-1][:-4]
+        img = img_file['arr_0']
+        plt.title('Classified as anomaly, Quality rating = {}'.format(quality))
+        plt.xlabel('File name: {}'.format(f))
+        plt.imshow(img, cmap='hot')
+        plt.show()
+
+
 def generate_distributions(name):
     data = np.load('{}.npz'.format(name))
     classifications = data['classifications']
@@ -13,7 +62,7 @@ def generate_distributions(name):
     neg_ratings = neg_classifications[:, 1]
     pos_kde = stats.gaussian_kde(pos_ratings)
     neg_kde = stats.gaussian_kde(neg_ratings)
-    x = np.linspace(0, 1.0, 1000)
+    x = np.linspace(0, 1.0, 500)
     pos_dens = pos_kde.evaluate(x)
     neg_dens = neg_kde.evaluate(x)
     fig = plt.figure()
@@ -36,7 +85,7 @@ def generate_distributions(name):
 
 
 def main():
-    generate_distributions('Fred')
+    check_errors('./Flavie/Flavie_fixed')
 
 
 if __name__ == "__main__":
